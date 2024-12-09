c764b135d3466ee04e44c67853d9009b6b97837e
Evaldas Lupeikis <levaldas@hotmail.com>
2024-12-09 21:51:25 +0200

    Final vynoteka.lt autotests

--- src/test/java/VynotekaTests.java
+++ src/test/java/VynotekaTests.java
@@ -26,11 +26,14 @@ public class VynotekaTests {
         _globalDriver.manage().window().maximize();
         _globalDriver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
         WebDriverWait wait = new WebDriverWait(_globalDriver, Duration.ofSeconds(60));
-        WebElement acceptAgeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div[3]/div/div[1]/button")));
+        WebElement acceptAgeButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("/html/body/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div[3]/div/div[1]/button")));
         acceptAgeButton.click();
-        WebElement acceptCookiesButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[1]/div/div[1]/a[2]")));
+        WebElement acceptCookiesButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("/html/body/div[1]/div[1]/div/div[1]/a[2]")));
         acceptCookiesButton.click();
-        WebElement popupCloseButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[4]/div[1]/div/div[2]/div[2]/div[2]/button")));
+        WebElement popupCloseButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("/html/body/div[4]/div[1]/div/div[2]/div[2]/div[2]/button")));
         popupCloseButton.click();
     }
     @AfterTest
@@ -90,12 +93,83 @@ public class VynotekaTests {
         //Vartotojo informacija
         _globalDriver.findElement(By.cssSelector(".user-actions__link--login")).click();
         Thread.sleep(1000);
-        String vardasPavarde = _globalDriver.findElement(By.xpath("/html/body/div[1]/div[1]/main/section/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/span")).getText();
+        String vardasPavarde = _globalDriver.findElement(
+                By.xpath("/html/body/div[1]/div[1]/main/section/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/span")).getText();
         Assert.assertEquals(vardasPavarde,"Juozas Antanas");
         Thread.sleep(1000);
     }
 
+    //2.Testas
+    // tikrinama vartotojo prisijungimas- turi matytis asmeninė infomacija, užregistruoto vartotojo vardas ir pavardė
+    @Test
+    public void test2() throws InterruptedException {
+        //_globalDriver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        Thread.sleep(1000);
+        //Spaudžiame Prisijungti
+        _globalDriver.findElement(By.xpath("/html/body/div[2]/div[1]/header/div[2]/div/div/div[4]/nav/div[1]/button")).click();
+        Thread.sleep(1000);
+        _globalDriver.findElement(
+                By.xpath("//input[@placeholder='El. paštas']")).sendKeys("juozas.antanas9@gmail.com");
+        Thread.sleep(1000);
+        _globalDriver.findElement(
+                By.xpath("//input[@placeholder='Slaptažodis']")).sendKeys("JuozasAntanas0");
+        Thread.sleep(1000);
+        //Prisijungti
+        //_globalDriver.findElement(By.xpath("//button[@class='btn primary full-w']")).click();
+        _globalDriver.findElement(By.xpath("//button[contains(text(), 'PRISIJUNGTI')]")).click();
+        Thread.sleep(1000);
+        //Vartotojo informacija
+        _globalDriver.findElement(By.cssSelector(".btn.user-actions__link.user-actions__link--login")).click();
+        Thread.sleep(1000);
+        //Tikrinimas
+        String actualText = _globalDriver.findElement(
+                By.xpath("/html/body/div[1]/div[1]/main/section/div/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/span")).getText();
+        String expectedText = "Juozas Antanas";
+        Assert.assertEquals(actualText, expectedText, "Vartotojo vardas ir pavardė nesutampa!");
+    }
 
+    //3.Testas
+    // tikrinama prekės įdėjimas į krepšelį- krepšelyje turi būti ieškoma prekė
+    @Test
+    public void test3() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(_globalDriver, Duration.ofSeconds(5));
+        _globalDriver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        //Paieškos laukelyje įvedame reikšmę
+        WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Pvz.: dovanos...']")));
+        inputField.sendKeys("Johann Brunner Premium Riesling");
+        //Spaudžiame į krepšelį
+        WebElement toCartButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Į KREPŠELĮ')]"))
+        );
+        toCartButton.click();
+        //Tikriname ar prekė krepšelyje
+        WebElement itemInCart = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//a[contains(@class, 'cart-item__title') and contains(text(), 'Johann Brunner Premium Riesling')]"))
+        );
+        Assert.assertNotNull(itemInCart,"Prekė su pavadinimu 'Johann Brunner Premium Riesling' nerasta krepšelyje.");
+    }
+
+    //4.Testas
+    // tikrinama prekės informacinis langas ar pritaikoma prekei kainos nuolaida
+    @Test
+    public void test4() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(_globalDriver, Duration.ofSeconds(5));
+        _globalDriver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        //Paieškos laukelyje įvedame reikšmę
+        WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Pvz.: dovanos...']")));
+        inputField.sendKeys("Dallmayr Classic Intense");
+        //Spaudžiame ant surastos prekės
+        WebElement productTitle = _globalDriver.findElement(By.xpath("//a[@class='cart-item__title' and contains(span/text(), 'Dallmayr Classic Intense')]"));
+        productTitle.click();
+        //Tikriname ar teisinga kaina
+        WebElement priceIntElement = _globalDriver.findElement(By.xpath("//span[@class='product-price__int']"));
+        String priceInt = priceIntElement.getText();
+        WebElement priceDecimalElement = _globalDriver.findElement(By.xpath("//span[@class='product-price__decimal']"));
+        String priceDecimal = priceDecimalElement.getText();
+        String actualPrice = priceInt + "." + priceDecimal + " €";
+        String expectedPrice = "4.99 €";
+        Assert.assertEquals(actualPrice, expectedPrice, "Kaina už prekę nesutampa!");
+    }
 
 }
 
